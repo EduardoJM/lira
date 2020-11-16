a3576a8d9d67a4c8fb27712b4e8e194982ca0c30
Eduardo <eduardo_y05@outlook.com>
2020-11-16 10:34:05 -0200

    remove unused statistical code and add proportion sample size

--- packages/shared/survey/src/core/confiance/ProportionConservator.ts
+++ packages/shared/survey/src/core/confiance/ProportionConservator.ts
@@ -16,6 +16,19 @@ class ProportionConservator extends ConfianceInterval {
         const error = z / Math.sqrt(4 * sample.getSampleSize());
         return error;
     }
+
+    static getSampleSize(confiability: number, error: number): number {
+        if (confiability < 0 || confiability > 1) {
+            throw new Error('confiability must be a probability between 0 and 1.');
+        }
+        if (error < 0 || error > 1) {
+            throw new Error('error must be a probability between 0 and 1.');
+        }
+        const prob = (1 - confiability) / 2;
+        const z = Math.abs(ppf(0, 1, prob));
+        const n = Math.pow(z / error, 2) / 4;
+        return Math.ceil(n);
+    }
 }
 
 export default ProportionConservator;
