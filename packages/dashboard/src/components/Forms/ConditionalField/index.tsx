f0026736badb3614070c0320296dde5e6f226d3f
Eduardo <eduardo_y05@outlook.com>
2020-11-19 11:17:07 -0200

    feat(eslint): adjust files from @lira/dashboard and eslint configuration

--- packages/dashboard/src/components/Forms/ConditionalField/index.tsx
+++ packages/dashboard/src/components/Forms/ConditionalField/index.tsx
@@ -1,4 +1,9 @@
-import React, { useState, useRef, useEffect, ChangeEvent } from 'react';
+import React, {
+    useState,
+    useRef,
+    useEffect,
+    ChangeEvent,
+} from 'react';
 import { Box, FormControlLabel, Switch } from '@material-ui/core';
 import { useField } from '@unform/core';
 
@@ -10,21 +15,24 @@ interface ConditionalFieldProps {
 }
 
 const ConditionalField: React.FC<ConditionalFieldProps> = (props) => {
-    const { name, label, initialValue, field } = props;
+    const {
+        name,
+        label,
+        initialValue,
+        field,
+    } = props;
     const fieldRef = useRef(null);
     const [state, setState] = useState(initialValue);
-    const { fieldName, defaultValue, registerField, error } = useField(name);
+    const { fieldName, registerField } = useField(name);
 
     useEffect(() => {
         registerField({
             name: fieldName,
             ref: fieldRef.current,
-            getValue: (ref: any) => {
-                return state;
-            },
+            getValue: () => state,
             setValue: (ref: any, check: boolean) => {
                 setState(check);
-            }
+            },
         });
     }, [fieldName, registerField, state]);
 
@@ -36,13 +44,13 @@ const ConditionalField: React.FC<ConditionalFieldProps> = (props) => {
         <>
             <Box>
                 <FormControlLabel
-                    control={
+                    control={(
                         <Switch
                             checked={state}
                             onChange={handleChange}
                             name={name}
                         />
-                    }
+                    )}
                     label={label}
                 />
             </Box>
--- packages/dashboard/src/components/Forms/IntegerField/index.tsx
+++ packages/dashboard/src/components/Forms/IntegerField/index.tsx
@@ -1,4 +1,9 @@
-import React, { useRef, useEffect, useState, ChangeEvent } from 'react';
+import React, {
+    useRef,
+    useEffect,
+    useState,
+    ChangeEvent,
+} from 'react';
 import { TextField, StandardTextFieldProps } from '@material-ui/core';
 import { useField } from '@unform/core';
 
@@ -11,24 +16,22 @@ const IntegerField: React.FC<IntegerFieldProps> = (props) => {
     const { name, label, ...others } = props;
     const inputRef = useRef(null);
     const [value, setValue] = useState('');
-    const { fieldName, defaultValue, registerField, error } = useField(name);
+    const { fieldName, defaultValue, registerField } = useField(name);
 
     useEffect(() => {
         registerField({
             name: fieldName,
             ref: inputRef.current,
-            getValue: (ref: any) => {
-                return value;
-            },
+            getValue: () => value,
             setValue: (ref: any, str: string) => {
                 setValue(str);
-            }
+            },
         });
     }, [fieldName, registerField, value]);
-    
+
     function handleChange(e: ChangeEvent<HTMLInputElement>) {
         const str = e.target.value as string;
-        const num = parseInt(str);
+        const num = parseInt(str, 10);
         setValue(num.toString(10));
     }
 
