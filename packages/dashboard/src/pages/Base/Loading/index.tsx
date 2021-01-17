48c2ff1c6b0813d5dd01eabd005f55b52efe959e
Eduardo Oliveira <eduardo_y05@outlook.com>
2021-01-16 23:24:09 -0300

    add circular progress animation to loading page

--- packages/dashboard/src/pages/Base/Loading/index.tsx
+++ packages/dashboard/src/pages/Base/Loading/index.tsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import { CircularProgress } from '@material-ui/core';
 
-const Loading: React.FC = () => <h1>Carregando...</h1>;
+const Loading: React.FC = () => {
+    return (
+        <div style={{
+            position: 'fixed',
+            left: 0,
+            top: 0,
+            right: 0,
+            bottom: 0,
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center'
+        }}>
+            <CircularProgress />
+        </div>
+    );
+}
 
 export default Loading;
